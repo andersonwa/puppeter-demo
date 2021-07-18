9ec43e73fc459885efc11fe6037f39fb0fe31c06
Anderson Dario <andersonwebwa@hotmail.com>
2021-07-18 19:11:53 -0300

    added try catch validation

--- index.js
+++ index.js
@@ -4,54 +4,60 @@ import {loginSelectors, searchResultSelectors, pages, searchParam} from './helpe
 
 (async () => {
   dotenv.config();
-  const browser = await puppeteer.launch({
-    args: ['--no-sandbox'],
-    timeout: 10000,
-  });
-  const page = await browser.newPage();
 
-  await page.goto(pages.loginPage);
+  try {
+    const browser = await puppeteer.launch({
+      args: ['--no-sandbox'],
+      timeout: 10000,
+    });
+    
+    const page = await browser.newPage();
 
-  await page.type(loginSelectors.usernameInput, process.env.USER_LOGIN);
-  await page.type(loginSelectors.passwordInput, process.env.DATABASE_HOST);
-  await page.click(loginSelectors.submitButton);
-  await page.waitForSelector(loginSelectors.loggedInProfile);
+    await page.goto(pages.loginPage);
 
-  await page.goto(`${pages.searchPage}/${searchParam.query}`);
+    await page.type(loginSelectors.usernameInput, process.env.USER_LOGIN);
+    await page.type(loginSelectors.passwordInput, process.env.DATABASE_HOST);
+    await page.click(loginSelectors.submitButton);
+    await page.waitForSelector(loginSelectors.loggedInProfile);
 
-  console.log(loginSelectors, searchResultSelectors);
+    await page.goto(`${pages.searchPage}/${searchParam.query}`);
 
-  await page.waitForSelector(searchResultSelectors.resultDiv);
+    console.log(loginSelectors, searchResultSelectors);
 
-  const data = await page.evaluate((searchResultSelectors) => {
-    let legendas = [];
-    const resultElements = Array.from(document.querySelectorAll(searchResultSelectors.resultItems));
+    await page.waitForSelector(searchResultSelectors.resultDiv);
 
-    resultElements.forEach((resultElement) => {
-      const language = resultElement.querySelector(searchResultSelectors.languageImage).title;
-      const name = resultElement.querySelector(searchResultSelectors.itemName).innerText;
-      const downloadUrl = resultElement.querySelector(searchResultSelectors.itemDownloadLink).href;
-      const documentText = resultElement.querySelector(searchResultSelectors.itemDescription).innerText;
+    const data = await page.evaluate((searchResultSelectors) => {
+      let legendas = [];
+      const resultElements = Array.from(document.querySelectorAll(searchResultSelectors.resultItems));
 
-      const splitedText = documentText.split(' ');
-      const score = Number(splitedText[3].replace(",", ""));
-      const createDate = new Date(splitedText[8] + ' ' + splitedText[10]).toJSON();
+      resultElements.forEach((resultElement) => {
+        const language = resultElement.querySelector(searchResultSelectors.languageImage).title;
+        const name = resultElement.querySelector(searchResultSelectors.itemName).innerText;
+        const downloadUrl = resultElement.querySelector(searchResultSelectors.itemDownloadLink).href;
+        const documentText = resultElement.querySelector(searchResultSelectors.itemDescription).innerText;
 
-      legendas.push({
-        name: name,
-        downloadQuantity: Number(splitedText[0]),
-        score: score,
-        likeRatio: 2.5,
-        author: splitedText[6],
-        date: createDate,
-        language: language,
-        downloadUrl:  downloadUrl
+        const splitedText = documentText.split(' ');
+        const score = Number(splitedText[3].replace(",", ""));
+        const createDate = new Date(splitedText[8] + ' ' + splitedText[10]).toJSON();
+
+        legendas.push({
+          name: name,
+          downloadQuantity: Number(splitedText[0]),
+          score: score,
+          likeRatio: 2.5,
+          author: splitedText[6],
+          date: createDate,
+          language: language,
+          downloadUrl:  downloadUrl
+        })
       })
-    })
 
-    return legendas;
-  }, searchResultSelectors);
-  
-  console.log(`Foram encontradas ${data.length} legendas`);
-  await browser.close();
+      return legendas;
+    }, searchResultSelectors);
+    
+    console.log(`Foram encontradas ${data.length} legendas`);
+    await browser.close();
+  } catch (e) {
+    console.log(e);
+  }
 })();
